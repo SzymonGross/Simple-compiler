4001dc3c0987095dd95faf4266598aa1581a6046
Szymon <167433638+SzymonGross@users.noreply.github.com>
2026-06-07 13:21:05 +0200

    used improved syntax

--- kody_pl/p2.pl
+++ kody_pl/p2.pl
@@ -1,20 +1,16 @@
-stworz liczba a
-ustaw a 3
+liczba a <- 3
+liczba b <- 7
 
-stworz liczba b
-ustaw b 7
-
-stworz liczba c
-ustaw c a*b
+liczba c <- a*b
 
 dopoki b > 0
-begin
-    ustaw b b-1
+{
+    b <- b-1
 
     jezeli a > b || b == 5
-    begin
-        ustaw a a+1
-    end
-end
+    {
+        a <- a+1
+    }
+}
 
 zakoncz a
